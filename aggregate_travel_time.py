4f91e3ae29552887f033b573cfa146359dc127d9
Gan-Jiang <yl2464@cornell.edu>
2017-03-16 00:56:11 -0400

    complete travel_time processing and weather processing.
    There is still something to do with the weather tho since there are
    missing data and outliers.

--- aggregate_travel_time.py
+++ aggregate_travel_time.py
@@ -12,7 +12,16 @@ import math
 from datetime import datetime,timedelta
 
 file_suffix = '.csv'
-path = '../'  # set the data directory
+path = 'dataSets/training/'  # set the data directory
+
+def update_dic(travel_times, route_id):
+    start = datetime(2016, 9, 19, 0, 0)
+    end = datetime(2016, 10, 18, 0, 0)
+    while start < end:
+        travel_times[route_id][start] = [0]
+        start = start + timedelta(minutes=20)
+    return travel_times
+
 
 def avgTravelTime(in_file):
 
@@ -37,6 +46,7 @@ def avgTravelTime(in_file):
         route_id = intersection_id + '-' + tollgate_id
         if route_id not in travel_times.keys():
             travel_times[route_id] = {}
+            travel_times = update_dic(travel_times, route_id)
 
         trace_start_time = each_traj[3]
         trace_start_time = datetime.strptime(trace_start_time, "%Y-%m-%d %H:%M:%S")
@@ -44,25 +54,24 @@ def avgTravelTime(in_file):
         start_time_window = datetime(trace_start_time.year, trace_start_time.month, trace_start_time.day,
                                      trace_start_time.hour, time_window_minute, 0)
         tt = float(each_traj[-1]) # travel time
-
-        if start_time_window not in travel_times[route_id].keys():
-            travel_times[route_id][start_time_window] = [tt]
-        else:
-            travel_times[route_id][start_time_window].append(tt)
+        start = datetime(2016, 9, 19, 0, 0)
+        if start_time_window < start:
+            continue
+        travel_times[route_id][start_time_window].append(tt)
 
     # Step 3: Calculate average travel time for each route per time window
-    fw = open(out_file_name, 'w')
+    fw = open(path + out_file_name, 'w')
     fw.writelines(','.join(['"intersection_id"', '"tollgate_id"', '"time_window"', '"avg_travel_time"']) + '\n')
     for route in travel_times.keys():
         route_time_windows = list(travel_times[route].keys())
         route_time_windows.sort()
         for time_window_start in route_time_windows:
-            time_window_end = time_window_start + timedelta(minutes=20)
             tt_set = travel_times[route][time_window_start]
-            avg_tt = round(sum(tt_set) / float(len(tt_set)), 2)
-            out_line = ','.join(['"' + route.split('-')[0] + '"', '"' + route.split('-')[1] + '"',
-                                 '"[' + str(time_window_start) + ',' + str(time_window_end) + ')"',
-                                 '"' + str(avg_tt) + '"']) + '\n'
+            if len(tt_set) == 1:
+                avg_tt = 0
+            else:
+                avg_tt = round(sum(tt_set) / float(len(tt_set) - 1), 2)
+            out_line = ','.join(['"' + route.split('-')[0] + '"', '"' + route.split('-')[1] + '"','"' + str(time_window_start) + '"','"' + str(avg_tt) + '"']) + '\n'
             fw.writelines(out_line)
     fw.close()
 
---
+++ process_weather.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+"""
+Objective:
+Calculate the average travel time for each 20-minute time window.
+
+"""
+
+# import necessary modules
+import math
+from datetime import datetime,timedelta
+
+file_suffix = '.csv'
+path = 'dataSets/training/'  # set the data directory
+
+def update_dic(weathers):
+    start = datetime(2016, 9, 19, 0, 0)
+    end = datetime(2016, 10, 18, 0, 0)
+    while start < end:
+        weathers[start] = {}
+        start = start + timedelta(minutes=20)
+    return weathers
+
+
+def process_weather(in_file):
+
+    out_suffix = '_20min_weather'
+    in_file_name = in_file + file_suffix
+    out_file_name = in_file.split('_')[1] + out_suffix + file_suffix
+
+    # Step 1: Load trajectories
+    fr = open(path + in_file_name, 'r')
+    fr.readline()  # skip the header
+    wea_data = fr.readlines()
+    fr.close()
+    # Step 2: Create a dictionary to store travel time for each route per time window
+    weathers = {}
+    weathers = update_dic(weathers)
+    for i in range(len(wea_data)):
+        each_wea = wea_data[i].replace('"', '').split(',')
+        date = each_wea[0]
+        hour = each_wea[1]
+        pressure = each_wea[2]
+        sea_pressure = each_wea[3]
+        wind_direction = each_wea[4]
+        wind_speed = each_wea[5]
+        temperature = each_wea[6]
+        rel_humidity = each_wea[7]
+        precipitation = each_wea[8][:-2]
+
+        start_time_window = datetime.strptime(date + ' ' + hour + ':0:0', "%Y-%m-%d %H:%M:%S")
+
+        start = datetime(2016, 9, 19, 0, 0)
+        if start_time_window < start:
+            continue
+        start = start_time_window
+        end_time_window = start_time_window + timedelta(hours=3)
+        while (start < end_time_window):
+            weathers[start]['pressure'] = pressure
+            weathers[start]['sea_pressure'] = sea_pressure
+            weathers[start]['wind_direction'] = wind_direction
+            weathers[start]['wind_speed'] = wind_speed
+            weathers[start]['temperature'] = temperature
+            weathers[start]['rel_humidity'] = rel_humidity
+            weathers[start]['precipitation'] = precipitation
+            start = start + timedelta(minutes=20)
+    # Step 3: Calculate average travel time for each route per time window
+    fw = open(path + out_file_name, 'w')
+    fw.writelines(','.join(['"time"', '"pressure"', '"sea_pressure"', '"wind_direction"', '"wind_speed"', '"temperature"', '"rel_humidity"', '"precipitation"']) + '\n')
+    for time in weathers.keys():
+        try:
+            out_line = ','.join(['"' + str(time) + '"','"' + str(weathers[time]['pressure']) + '"','"' + str(weathers[time]['sea_pressure']) + '"','"' + str(weathers[time]['wind_direction']) + '"','"' + str(weathers[time]['wind_speed']) + '"','"' + str(weathers[time]['temperature']) + '"','"' + str(weathers[time]['rel_humidity']) + '"','"' + str(weathers[time]['precipitation']) + '"']) + '\n'
+        except:
+            aaa=1
+        fw.writelines(out_line)
+    fw.close()
+
+def main():
+
+    in_file = 'weather (table 7)_training'
+    process_weather(in_file)
+
+if __name__ == '__main__':
+    main()
+
+
+
